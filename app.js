a876cfa9a46b47491acaf18f1bc265e9f0f3febe
Thomas McNish <mcnishthomas@gmail.com>
2022-02-22 20:54:03 -0500

    fixed browser rendering issue. Ran PYTHON 3 command (not regular Python command)

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
-const title = React.createElement(
-    'h1',
-    { id: 'main-title', title: 'This is a title.' },
-    'Hello World'
-);
+const title = <h1>Hello World</h1>;
+const desc = <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris</p>;
 
-const desc = React.createElement(
-    'p',
-    null,
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris'
-);
 
 const header = React.createElement(
     'header',
